8ecebf6ee92658b11fc460eba6279667aba5b2a8
fightwing <2608379678@qq.com>
2020-09-14 21:40:30 +0800

    完成保龄球记分，待重构

---
+++ src/main/java/BowlingGame.java
@@ -0,0 +1,55 @@
+/**
+ * @author Boyu Yuan
+ * @date 2020/9/14 20:37
+ */
+public class BowlingGame {
+    public int rolls[] = new int[30];
+    public int rollIndex = 0;
+
+    public void roll(int mark){
+        rolls[rollIndex++] = mark;
+    }
+
+
+    public int calculateScore() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++){
+            score += rolls[i];
+        }
+        return score;
+    }
+
+
+    public int calculateFillInScore() {
+        int score = 0;
+        int index = 0;
+        for (int round = 0; round < 10; round++){
+            if (rolls[index]+rolls[index+1] == 10){
+                score += rolls[index+2]+10;
+                index = index + 2;
+            }else {
+                score += rolls[index] + rolls[index+1];
+                index = index + 2;
+            }
+        }
+        return score;
+    }
+
+    public int calculateAllInScore() {
+        int score = 0;
+        int index = 0;
+        for (int round = 0; round < 10; round++){
+            if (rolls[index] == 10){
+                score += 10 + rolls[index+1] + rolls[index+2];
+                index = index +1 ;
+            }else if  (rolls[index]+rolls[index+1] == 10){
+                score += rolls[index+2]+10;
+                index = index + 2;
+            }else {
+                score += rolls[index] + rolls[index+1];
+                index = index + 2;
+            }
+        }
+        return score;
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class BowlingGameTest {
+
+    BowlingGame game = new BowlingGame();
+    @Test
+    void should_return_sum_when_the_score_includes_all_in(){
+        for (int i = 0; i < 12; i++){
+            game.roll(10);
+        }
+        assertEquals(300,game.calculateAllInScore());
+
+
+    }
+    @Test
+    void should_return_sum_when_all_zero_score(){
+        for (int i = 0; i < 20; i++){
+            game.roll(0);
+        }
+        assertEquals(0,game.calculateScore());
+    }
+    @Test
+    void should_return_sum_when_the_score_includes_fill_in(){
+        game.roll(7);
+        game.roll(3);
+        for (int i = 0; i < 18; i++){
+            game.roll(1);
+        }
+        assertEquals(29,game.calculateFillInScore());
+    }
+
+
+
 }
